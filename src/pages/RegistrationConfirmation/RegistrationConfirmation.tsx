95cb2e84a464ea8c255f040205faca598e3ee435
Vladislav Domorad <vdomorad@solvd.com>
2023-05-29 21:04:03 +0200

    [Lesson-39]: Страница подтверждения регистрации

---
+++ src/pages/RegistrationConfirmation/RegistrationConfirmation.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import FormPagesContainer from "../../components/FormPagesContainer";
+
+const RegistrationConfirmation = () => {
+  return (
+    <FormPagesContainer
+      title={"Registration Confirmation"}
+      btnTitle={"Go to home"}
+      onSubmit={() => {}}
+    >
+      <div>
+        {
+          "Please activate your account with the activation link in the email example@gmail.com.\n Please, check your email"
+        }
+      </div>
+    </FormPagesContainer>
+  );
+};
+
+export default RegistrationConfirmation;
