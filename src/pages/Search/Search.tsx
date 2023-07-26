8f97218522db3d0c8382315e62aff2a6d4aaf72f
Vladislav Domorad <vdomorad@solvd.com>
2023-07-26 19:29:23 +0200

    [Lesson-53]: Pagination on more button click

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 import { RoutesList } from "src/pages/Router";
 import { getSearchedPosts, PostSelectors } from "src/redux/reducers/postSlice";
@@ -9,10 +8,8 @@ import Title from "src/components/Title";
 import PostCard, { CardTypes } from "src/components/Card";
 import { useCardActions } from "src/hooks";
 import EmptyState from "src/components/EmptyState";
-
-import styles from "./Search.module.scss";
 import { PER_PAGE } from "src/utils/constants";
-import Loader from "src/components/Loader";
+import Button, { ButtonTypes } from "src/components/Button";
 
 const Search = () => {
   const { search } = useParams();
@@ -42,33 +39,31 @@ const Search = () => {
   return (
     <div>
       <Title title={`Search results: "${search}"`} />
-      <div className={styles.container} id="scrollableDiv">
-        {searchedPosts.length ? (
-          <InfiniteScroll
-            next={onNextReached}
-            scrollThreshold={0.7}
-            hasMore={searchedPosts.length < totalPosts}
-            loader={<Loader />}
-            dataLength={searchedPosts.length}
-            scrollableTarget="scrollableDiv"
-          >
-            {searchedPosts.map((post) => {
-              return (
-                <PostCard
-                  type={CardTypes.Search}
-                  onMoreClick={onMoreClick(post)}
-                  {...post}
-                />
-              );
-            })}
-          </InfiniteScroll>
-        ) : (
-          <EmptyState
-            title={"Nothing was found..."}
-            description={"Try another search request"}
-          />
-        )}
-      </div>
+      {searchedPosts.length ? (
+        <div>
+          {searchedPosts.map((post) => {
+            return (
+              <PostCard
+                type={CardTypes.Search}
+                onMoreClick={onMoreClick(post)}
+                {...post}
+              />
+            );
+          })}
+          {searchedPosts.length < totalPosts && (
+            <Button
+              type={ButtonTypes.Secondary}
+              title={"More"}
+              onClick={onNextReached}
+            />
+          )}
+        </div>
+      ) : (
+        <EmptyState
+          title={"Nothing was found..."}
+          description={"Try another search request"}
+        />
+      )}
     </div>
   );
 };
